0761431f891d05ffb95c3f9b049f94c62de2f1c9
Marcos <exegeses@gmail.com>
2024-05-04 12:28:14 -0300

    Complicación adicional trayendo datos de la tabla pivote

--- clase5/consulta-productos-proveedores.sql
+++ clase5/consulta-productos-proveedores.sql
@@ -13,5 +13,31 @@ SELECT
       ON proveedores.idProveedor = productos_proveedores.idProveedor
     ORDER BY productos.idProducto;
 
+/* Agregamos el nombre de la marca (de la tabla marcas)
+   y el nombre de la categoría (de la tabla categorías) */
+SELECT
+        prdNombre AS Producto,
+        mkNombre AS Marca,
+        catNombre AS Categoría,
+        prvNombre AS Proveedor
+    FROM productos
+      JOIN marcas
+       ON productos.idMarca = marcas.idMarca
+      JOIN categorias
+       ON productos.idCategoria = categorias.idCategoria
+      JOIN productos_proveedores
+       ON productos.idProducto = productos_proveedores.idProducto
+      JOIN proveedores
+       ON proveedores.idProveedor = productos_proveedores.idProveedor
+    ORDER BY productos.idProducto;
+
 
 -- utilizando la técnica table relation
+SELECT
+    prdNombre AS Producto,
+    prvNombre AS Proveedor
+  FROM productos, proveedores, productos_proveedores
+  WHERE productos.idProducto = productos_proveedores.idProducto
+    AND proveedores.idProveedor = productos_proveedores.idProveedor
+ORDER BY productos.idProducto;
+
